f005b6593abd7e4e80eb566f31544dfe6499ae77
latronixo <pamerra@mail.ru>
2025-02-05 11:33:41 +0300

    Сделал связь элементов сториборда с кодом в ViewController

--- 96.Quizzler/ViewController.swift
+++ 96.Quizzler/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var progressView: UIProgressView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
