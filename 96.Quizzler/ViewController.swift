db705e58dca0c84814183e8def0bf6ee56962218
latronixo <pamerra@mail.ru>
2025-02-05 10:10:22 +0300

    Initial Commit

---
+++ 96.Quizzler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  96.Quizzler
+//
+//  Created by Валентин Картошкин on 05.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
